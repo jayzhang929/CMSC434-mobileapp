5bd0c6abef085287f8d927d5d4c5c2367a6224f7
Jay Zhang <jayzhang@Jays-MacBook-Air.local>
2016-05-01 20:04:21 -0400

    style: bullets point added to convert all paragraphs

--- app/src/main/java/edu/umd/cs/tabby/NotificationHistory.java
+++ app/src/main/java/edu/umd/cs/tabby/NotificationHistory.java
@@ -7,8 +7,8 @@ import android.widget.ListView;
 
 public class NotificationHistory extends AppCompatActivity {
 
-    static final String[] titles = {"Lookup Polling Location", "7 Days until Election Day"};
-    static final String[] dates = {"04-18-2016", "04-12-2016"};
+    static final String[] titles = {"Lookup Polling Location", "167 Days until Election Day", "174 Days until Election Day", "Check new Voter ID Law", "Hillary's Book came out"};
+    static final String[] dates = {"04-18-2016", "04-12-2016", "04-05-16", "03-21-16", "03-09-16"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
